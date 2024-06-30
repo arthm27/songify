805c767d9a576a467f6f0126d298f334b8b16736
arth <piyushmishra780720@gmail.com>
2024-06-30 11:19:49 +0530

    add create playlist and update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,69 +20,6 @@ function App() {
   const resultOffset = musicContext.resultOffset;
   const setResultOffset = musicContext.setResultOffset;
 
-  const fetchMusicData = async () => {
-    setTracks([]);
-    window.scrollTo(0, 0);
-    setIsLoading(true);
-    try {
-      const response = await fetch(
-        `https://api.spotify.com/v1/search?q=${keyword}&type=track&offset=${resultOffset}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch music data");
-      }
-
-      const jsonData = await response.json();
-
-      setTracks(jsonData.tracks.items);
-    } catch (error) {
-      setMessage(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      setResultOffset(0);
-      fetchMusicData();
-    }
-  };
-
-  useEffect(() => {
-    initializePlaylist();
-    const fetchToken = async () => {
-      try {
-        const response = await fetch("https://accounts.spotify.com/api/token", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: "grant_type=client_credentials&client_id=a77073181b7d48eb90003e3bb94ff88a&client_secret=68790982a0554d1a83427e061e371507",
-        });
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch token");
-        }
-
-        const jsonData = await response.json();
-        setToken(jsonData.access_token);
-      } catch (error) {
-        setMessage(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchToken();
-    setLikedMusic(JSON.parse(localStorage.getItem("likedMusic")));
-    setpinnedMusic(JSON.parse(localStorage.getItem("pinnedMusic")));
-  }, [setIsLoading, setLikedMusic, setpinnedMusic]);
 
   return (
     <>
@@ -110,31 +47,7 @@ function App() {
             return <Card key={element.id} element={element} />;
           })}
         </div>
-        <div className="row" hidden={tracks.length === 0}>
-          <div className="col">
-            <button
-              onClick={() => {
-                setResultOffset((previous) => previous - 20);
-                fetchMusicData();
-              }}
-              className="btn btn-outline-success w-100"
-              disabled={resultOffset === 0}
-            >
-              Previous Next Page: {resultOffset / 20}
-            </button>
-          </div>
-          <div className="col">
-            <button
-              onClick={() => {
-                setResultOffset((previous) => previous + 20);
-                fetchMusicData();
-              }}
-              className="btn btn-outline-success w-100"
-            >
-              Next Page: {resultOffset / 20 + 2}
-            </button>
-          </div>
-        </div>
+        
         <div className="row">
           <div className="col">
             <h4 className="text-center text-danger py-2">{message}</h4>
